9e7b9cca6f15c66abf7fee31b0a8afb3a4f883b8
AbelPaul <abelpaul12@gmail.com>
2023-12-21 13:32:21 +0100

    changed ebs

--- resources/views/layouts/toolbar.blade.php
+++ resources/views/layouts/toolbar.blade.php
@@ -5,7 +5,7 @@
         <!--begin::Page title-->
         <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
             <!--begin::Title-->
-            <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">NSITF EBS
+            <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">NIWA EBS
             </h1>
             <!--end::Title-->
             <!--begin::Breadcrumb-->
