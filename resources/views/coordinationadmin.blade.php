2bb6dce3ddbc1076e591a4981932e073a702a246
AbelPaul <abelpaul12@gmail.com>
2024-01-26 16:41:17 +0100

    done for today

---
+++ resources/views/coordinationadmin.blade.php
@@ -0,0 +1,183 @@
+@extends('layouts.app')
+@section('content')
+ <!-- partial -->
+ <div class="container">
+    <div class="content-wrapper">
+
+        <div class="row">
+            <div class="col-md-3 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-row">
+                            <img src="../images/faces/face11.html" class="img-lg rounded" alt="profile image"/>
+                            <div class="ml-3">
+                                <h6>Maria</h6>
+                                <p class="text-muted">maria@gmail.com</p>
+                                <p class="mt-2 text-primary font-weight-bold">Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-row">
+                            <img src="../images/faces/face9.jpg" class="img-lg rounded" alt="profile image"/>
+                            <div class="ml-3">
+                                <h6>Thomas Edison</h6>
+                                <p class="text-muted">thomas@gmail.com</p>
+                                <p class="mt-2 text-primary font-weight-bold">Developer</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-row">
+                            <img src="../images/faces/face12.jpg" class="img-lg rounded" alt="profile image"/>
+                            <div class="ml-3">
+                                <h6>Edward</h6>
+                                <p class="text-muted">edward@gmail.com</p>
+                                <p class="mt-2 text-primary font-weight-bold">Tester</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-row">
+                            <img src="../images/faces/face12.jpg" class="img-lg rounded" alt="profile image"/>
+                            <div class="ml-3">
+                                <h6>Edward</h6>
+                                <p class="text-muted">edward@gmail.com</p>
+                                <p class="mt-2 text-primary font-weight-bold">Tester</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row">
+
+            <div class="col-md-4 grid-margin stretch-card">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <img src="../images/faces/face5.jpg" class="img-lg rounded-circle mb-2" alt="profile image"/>
+                        <h4>Maria Johnson</h4>
+                        <p class="text-muted">Developer</p>
+                        <p class="mt-4 card-text">
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                                Aenean commodo ligula eget dolor. Lorem
+                        </p>
+                        <button class="btn btn-info btn-sm mt-3 mb-4">Follow</button>
+                        <div class="border-top pt-3">
+                            <div class="row">
+                                <div class="col-4">
+                                    <h6>5896</h6>
+                                    <p>Post</p>
+                                </div>
+                                <div class="col-4">
+                                    <h6>1596</h6>
+                                    <p>Followers</p>
+                                </div>
+                                <div class="col-4">
+                                    <h6>7896</h6>
+                                    <p>Likes</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 grid-margin stretch-card">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <img src="../images/faces/face5.jpg" class="img-lg rounded-circle mb-2" alt="profile image"/>
+                        <h4>Maria Johnson</h4>
+                        <p class="text-muted">Developer</p>
+                        <p class="mt-4 card-text">
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                                Aenean commodo ligula eget dolor. Lorem
+                        </p>
+                        <button class="btn btn-info btn-sm mt-3 mb-4">Follow</button>
+                        <div class="border-top pt-3">
+                            <div class="row">
+                                <div class="col-4">
+                                    <h6>5896</h6>
+                                    <p>Post</p>
+                                </div>
+                                <div class="col-4">
+                                    <h6>1596</h6>
+                                    <p>Followers</p>
+                                </div>
+                                <div class="col-4">
+                                    <h6>7896</h6>
+                                    <p>Likes</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Updates</h4>
+                        <ul class="solid-bullet-list">
+                            <li>
+                                <h6>User confirmation</h6>
+                                <p>Lorem Ipsum is simply dummy text of the printing </p>
+                                <p class="text-muted">
+                                    <i class="far fa-clock"></i>
+                                    7 months ago.
+                                </p>
+                            </li>
+                            <li>
+                                <h6>Continuous evaluation</h6>
+                                <p>Lorem Ipsum is simply dummy text of the printing </p>
+                                <p class="text-muted">
+                                    <i class="far fa-clock"></i>
+                                    7 months ago.
+                                </p>
+                            </li>
+                            <li>
+                                <h6>Promotion</h6>
+                                <p>Lorem Ipsum is simply dummy text of the printing </p>
+                                <p class="text-muted">
+                                    <i class="far fa-clock"></i>
+                                    7 months ago.
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+    </div>
+    <!-- content-wrapper ends -->
+    <!-- partial:../partials/_footer.html -->
+    <footer class="footer">
+      <div class="d-sm-flex justify-content-center justify-content-sm-between">
+        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2018 <a href="https://www.urbanui.com/" target="_blank">Urbanui</a>. All rights reserved.</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="far fa-heart text-danger"></i></span>
+      </div>
+    </footer>
+    <!-- partial -->
+  </div>
+  <!-- main-panel ends -->
+
+
+
+@endsection
