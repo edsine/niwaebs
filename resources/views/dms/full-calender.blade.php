d4ea6da76e9100765792059212b4efb9be04c825
AbelPaul <abelpaul12@gmail.com>
2024-03-26 14:03:57 +0100

    overview done

--- resources/views/dms/full-calender.blade.php
+++ resources/views/dms/full-calender.blade.php
@@ -8,9 +8,9 @@
     <title>Full Claender</title>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" /> --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
