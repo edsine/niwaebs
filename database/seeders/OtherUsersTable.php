f6cc861b197d3ea6b1568b9b206368c1fe28c8db
AbelPaul <abelpaul12@gmail.com>
2024-04-02 10:50:57 +0100

    done with the managing director seeding

---
+++ database/seeders/OtherUsersTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class OtherUsersTable extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->first_name = 'Alh Munirudeen';
+        $user->last_name = 'Bola';
+        $user->middle_name = 'Oyebamiji';
+        $user->email = 'md@niwa.com';
+        $user->type = 'company';
+        $user->password = bcrypt('12345678');
+        $user->email_verified_at = now();
+        $user->save();
+
+        $user->createToken(Str::slug(config('app.name').'_auth_token', '_'))->plainTextToken;
+
+        // Assign Super Admin role
+
+        $mdrole = Role::where('name', '=', 'Managing Director')->first();
+
+        $user->assignRole($mdrole);
+    }
+}
