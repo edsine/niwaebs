96259be0e4b57a23f6166b9d4e0f19b0991a0432
Abdul5050 <tacticshustle@gmail.com>
2024-04-05 09:07:09 +0100

    approve documents fixed

---
+++ resources/views/documents1/create.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.app')
+
+@section('title', 'Sub-Service')
+
+
+@push('styles')
+@endpush
+
+
+@section('content')
+
+    {{-- <div class="components-preview wide-md mx-auto"> --}}
+    <div class="nk-block-head nk-block-head-sm">
+        <div class="nk-block-between">
+            <div class="nk-block-head-content">
+                <h3 class="nk-block-title page-title">Documents Upload</h3>
+                <div class="nk-block-des text-soft">
+                    <p>Upload documents and make payments</p>
+                </div>
+            </div><!-- .nk-block-head-content -->
+            <!-- .nk-block-head-content -->
+        </div><!-- .nk-block-between -->
+    </div><!-- .nk-block-head -->
+    <?php
+                   $userId = \Auth::id(); // Get the authenticated user's ID
+$userPayment = \App\Models\Payment::where('payment_type',4)->where('employer_id', auth()->user()->id)->where('service_id','!=', null)->latest()->first();
+     if(!empty($userPayment) && $userPayment->document_uploads == 1){
+                   ?>
+    <div class="nk-block nk-block-lg">
+        <div class="card card-bordered card-preview">
+            <div class="card-inner">
+                <form action="{{ route('documents.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @include('documents.form')
+                </form>
+            </div>
+        </div><!-- .card-preview -->
+    </div> <!-- nk-block -->
+    <?php }else{
+        ?> 
+        <div class="form-group">
+            <label for="" class="">You can not upload documents till you make a new application fee & processing fee
+                payments for a service.</label>
+                <br/>
+                <a class="btn btn-primary me-n1" href="{{route('payment.index')}}">Make Application Fees Payments</a>
+        </div>
+        <?php
+    } ?>
+    {{-- </div><!-- .components-preview --> --}}
+
+@endsection
+
+
+@push('scripts')
+   {{--  <script>
+        $(document).ready(function() {
+            //FETCH LGAs FROM STATE ID
+            const lUrl = "{{ route('employer.lgas') }}?state=";
+            $('#state_of_origin').change(function() {
+                $.ajax({
+                    url: lUrl + $(this).val(),
+                    type: "GET",
+                    data: null,
+                    dataType: 'json',
+                }).done(function(response) {
+                    $('#local_govt_area').empty();
+                    var lgas = '';
+                    $.each(response.data, function(a, b) {
+                        lgas += '<option value="' + b.id + '">' + b.name + '</option>';
+                    });
+                    $('#local_govt_area').html(lgas);
+                });
+            });
+
+            $('#state_of_origin').trigger('change');
+        });
+    </script> --}}
+@endpush
---
+++ resources/views/documents1/form.blade.php
@@ -0,0 +1,139 @@
+<div class="preview-block">
+    <span class="preview-title-lg overline-title">Required Documents</span>
+   
+    <div class="row gy-4">
+        <div class="col-lg-6 col-sm-6">
+            <div class="form-group">
+                <label class="form-label" for="default-06">Title Document (pdf only: .pdf (2MB max size))
+                </label>
+                <div class="form-control-wrap">
+                    <div class="form-file">
+                        <input type="hidden" name="title_document" value="Title Document"/>
+                        <input type="file" class="form-file-input" id="title_document_file"
+                            name="title_document_file" required>
+                        <label class="form-file-label" for="customFile">Choose file</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-sm-6">
+            <div class="form-group">
+                <label class="form-label" for="default-06">Survey document (pdf only: .pdf (2MB max size))
+                </label>
+                <div class="form-control-wrap">
+                    <div class="form-file">
+                        <input type="hidden" name="survey_document" value="Survey document"/>
+                        <input type="file" class="form-file-input" id="survey_document_file"
+                            name="survey_document_file" required>
+                        <label class="form-file-label" for="customFile">Choose file</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-sm-6">
+            <div class="form-group">
+                <label class="form-label" for="default-06">Sand search report (pdf only: .pdf (2MB max size))
+                </label>
+                <div class="form-control-wrap">
+                    <div class="form-file">
+                        <input type="hidden" name="sand_search_report" value="Sand search report"/>
+                        <input type="file" class="form-file-input" id="sand_search_report_file"
+                            name="sand_search_report_file" required>
+                        <label class="form-file-label" for="customFile">Choose file</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-sm-6">
+            <div class="form-group">
+                <label class="form-label" for="default-06">CAC certificate (pdf only: .pdf (2MB max size))
+                </label>
+                <div class="form-control-wrap">
+                    <div class="form-file">
+                        <input type="hidden" name="cac_certificate" value="CAC certificate"/>
+                        <input type="file" class="form-file-input" id="cac_certificate_file"
+                            name="cac_certificate_file" required>
+                        <label class="form-file-label" for="customFile">Choose file</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-sm-6">
+            <div class="form-group">
+                <label class="form-label" for="default-06">Pre & Post Dredge survey drawings (pdf only: .pdf (2MB max size))
+                </label>
+                <div class="form-control-wrap">
+                    <div class="form-file">
+                        <input type="hidden" name="pre_post_dredge_survey_drawings" value="Pre & Post Dredge survey drawings "/>
+                        <input type="file" class="form-file-input" id="pre_post_dredge_survey_drawings_file"
+                            name="pre_post_dredge_survey_drawings_file" required>
+                        <label class="form-file-label" for="customFile">Choose file</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-sm-6">
+            <div class="form-group">
+                <label class="form-label" for="default-06">E.I.A Report (pdf only: .pdf (2MB max size))
+                </label>
+                <div class="form-control-wrap">
+                    <div class="form-file">
+                        <input type="hidden" name="eia_report" value="E.I.A Report"/>
+                        <input type="file" class="form-file-input" id="eia_report_file"
+                            name="eia_report_file" required>
+                        <label class="form-file-label" for="customFile">Choose file</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-12 col-sm-12">
+            
+            <div class="form-group">
+                <label for="">Get the follwing documents
+                    as listed below in pdf format</label>
+                <span class="list text-muted">
+                    <ol>
+                        {{-- <li>Title document</li> --}}
+                        <div class="row">
+                            <div class="col-6">
+                                <li>Title document</li>
+                            </div>
+                            <div class="col-6">
+                                <li>Survey document</li>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-6">
+                                <li>Sand search report</li>
+                            </div>
+                            <div class="col-6">
+                                <li>CAC certificate</li>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-6">
+                                <li>Pre & Post Dredge survey drawings</li>
+                            </div>
+                            <div class="col-6">
+                                <li>E.I.A Report</li>
+                            </div>
+                        </div>
+                       
+                    </ol>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <hr class="preview-hr">
+    <div class="row g-4">
+        <div class="col-2">
+            <div class="form-group">
+                <button type="submit" class="btn btn-lg btn-primary btn-block"><em
+                        class="icon ni ni-save me-2"></em> SUBMIT</button>
+            </div>
+        </div>
+    </div>
+</div>
