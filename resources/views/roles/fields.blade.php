d9710c8d842ee66ece3e01c6cf9ec23337738e52
Omeiza Alabi <omeizaalabi@gmail.com>
2024-04-12 14:48:17 +0100

    Permission grouping

--- resources/views/roles/fields.blade.php
+++ resources/views/roles/fields.blade.php
@@ -8,10 +8,20 @@
     {!! Form::label('permissions[]', 'Permissions:') !!}
     <div class="flex flex-wrap justify-start mb-4">
         <div class="row">
-            @foreach ($permissions as $permission)
-                <div class="col-6 mb-3">
-                    {!! Form::checkbox('permissions[]', $permission->id, ['class' => 'form-control form-checkbox h-4 w-4', 'checked' => $permission->assigned]) !!}
-                    <span class="">{{ $permission->name }}</span>
+
+
+            @foreach ($groupedPermissions as $groupName => $permissions)
+                <div class="row">
+                    <h4 class="mb-3 mt-5">{{ $groupName }}</h4>
+                    @foreach ($permissions as $permission)
+                        <div class="col-6 mb-3">
+                            {!! Form::checkbox('permissions[]', $permission->id, [
+                                'class' => 'form-control form-checkbox h-4 w-4',
+                                'checked' => $permission->assigned,
+                            ]) !!}
+                            <span class="">{{ $permission->name }}</span>
+                        </div>
+                    @endforeach
                 </div>
             @endforeach
         </div>
--- resources/views/roles/show_fields.blade.php
+++ resources/views/roles/show_fields.blade.php
@@ -7,7 +7,7 @@
 <!-- Permissions Field -->
 <div class="col-sm-12">
     {!! Form::label('permissions', 'Permissions:') !!}
-    <div class="row">
+    {{-- <div class="row">
         <p>
             @foreach ($role_permissions as $permission)
                 <div class="col-sm-6">
@@ -15,5 +15,15 @@
                 </div>
             @endforeach
         </p>
-    </div>
+    </div> --}}
+    @foreach ($groupedPermissions as $groupName => $permissions)
+        <div class="row">
+            <h4 class="mb-3 mt-5">{{ $groupName }}</h4>
+            @foreach ($permissions as $permission)
+                <div class="col-sm-6">
+                    <li class="text-gray-800 list-disc">{{ $permission->name }}</li>
+                </div>
+            @endforeach
+        </div>
+    @endforeach
 </div>
