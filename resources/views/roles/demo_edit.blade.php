711ec4005ae8ed78d40f740cd72836e5cc7d0f8b
Abdul5050 <tacticshustle@gmail.com>
2024-04-29 10:26:38 +0100

    Update roles

--- resources/views/roles/demo_edit.blade.php
+++ resources/views/roles/demo_edit.blade.php
@@ -23,7 +23,7 @@
 
             <div class="card-body">
                 <div class="row">
-                    @include('roles.fields')
+                    @include('roles.fields_old')
                 </div>
             </div>
 
