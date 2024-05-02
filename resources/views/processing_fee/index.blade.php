9c476c291c5f7d5b4c01b2841eed9ecb4276cefb
Abdul5050 <tacticshustle@gmail.com>
2024-05-02 09:50:03 +0100

    remove delete button

--- resources/views/processing_fee/index.blade.php
+++ resources/views/processing_fee/index.blade.php
@@ -53,12 +53,12 @@
                                             class="nk-menu-icon text-info"><em class="fa fa-edit"></em></span></a>
                                     
 
-                                    <a id="delete-service" title="Terminate Service" style="cursor: pointer;"
+                                    {{-- <a id="delete-service" title="Terminate Service" style="cursor: pointer;"
                                         onclick="event.preventDefault();
                                     document.getElementById('delete-service-form').submit();"><span
                                             class="nk-menu-icon text-danger eg-swal-av3"><em
                                                 class="fa fa-trash"></em></span>
-                                    </a>
+                                    </a> --}}
                                     <form id="delete-service-form" action="{{ route('processing_fee.destroy', $processing_fee->id) }}"
                                         method="POST" class="d-inline">
                                         @csrf
--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -55,12 +55,12 @@
                                                 class="icon ni ni-trash"></em></span>
                                             </a> --}}
 
-                                    <a id="delete-service" title="Terminate Service" style="cursor: pointer;"
+                                    {{-- <a id="delete-service" title="Terminate Service" style="cursor: pointer;"
                                         onclick="event.preventDefault();
                                     document.getElementById('delete-service-form').submit();"><span
                                             class="nk-menu-icon text-danger eg-swal-av3"><em
                                                 class="fa fa-trash"></em></span>
-                                    </a>
+                                    </a> --}}
                                     <form id="delete-service-form" action="{{ route('services.destroy', $service->id) }}"
                                         method="POST" class="d-inline">
                                         @csrf
